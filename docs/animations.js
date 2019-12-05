3f9aee94157a56453e5878bdf232c4cb62988c94
Dani05 <dani229@abv.bg>
2019-12-05 16:27:37 +0200

    Index and footer ready, added some animations

---
+++ docs/animations.js
@@ -0,0 +1,25 @@
+var isInViewport = function (elem) {
+    var bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+
+var cards = document.getElementsByClassName('card')
+window.addEventListener('scroll', function (event) {
+    for (let index = 0; index < cards.length; index++) {
+        const card = cards[index];
+        if(isInViewport(card)){
+            if(index % 2 == 0){
+                card.classList.add('animation-right')
+            }
+            else{
+                card.classList.add('animation-left')
+            }
+        }
+    }
+
+}, false);
