20d610b413ce8b36c073bf35bf545f8d9b0522dd
Dani05 <dani229@abv.bg>
2019-12-09 17:18:37 +0200

    Added banner to Gallery and Tuning, fixed some mobile issues

--- docs/animations.js
+++ docs/animations.js
@@ -8,35 +8,63 @@ var isInViewport = function (elem) {
     );
 };
 
-var cards = document.getElementsByClassName('card-presentation')
-var cols = document.getElementsByClassName('col')
-var h4_suggestion = document.getElementById('suggestion')
+var path = window.location.pathname.split("/");
+var page = path[path.length - 2]
+
+if(page === ""){
+    var cards = document.getElementsByClassName('card-presentation')
+    var cols = document.getElementsByClassName('col')
+    var h4_suggestion = document.getElementById('suggestion')
+}
+if(page === "gallery"){
+    var photos = document.getElementById('gallery').children
+}
+
 
 window.addEventListener('scroll', function (event) {
-    for (let index = 0; index < cards.length; index++) {
-        const card = cards[index];
-        if(isInViewport(card)){
-            if(index % 2 == 0){
-                card.classList.add('slide-right')
-            }
-            else{
-                card.classList.add('slide-left')
-            }
-        }
-    }
-    for (let index = 0; index < cols.length; index++) {
-        const col = cols[index];
-        if(isInViewport(col)){
-            if(index % 2 == 0){
-                col.classList.add('slide-top')
-            }
-            else{
-                col.classList.add('slide-bottom')
+    if(page === ""){
+        for (let index = 0; index < cards.length; index++) {
+            const card = cards[index];
+            if(isInViewport(card)){
+                if(index % 2 == 0){
+                    card.classList.add('slide-right')
+                }
+                else{
+                    card.classList.add('slide-left')
+                }
             }
         }
+        for (let index = 0; index < cols.length; index++) {
+            const col = cols[index];
+            if(isInViewport(col)){
+                if(index % 2 == 0){
+                    col.classList.add('slide-top')
+                }
+                else{
+                    col.classList.add('slide-bottom')
+                }
+            }
+        }
+        if(isInViewport(h4_suggestion)){
+            h4_suggestion.classList.add('slide-top')
+        }
     }
-    if(isInViewport(h4_suggestion)){
-        h4_suggestion.classList.add('slide-top')
-    }
+    // if(page === "gallery"){
+    //     for (let index = 0; index < photos.length; index++) {
+    //         const photo = photos[index];
+    //         if(isInViewport(photo)){
+    //             if((index + 1) % 3 == 0){
+    //                 photo.classList.add('slide-bottom')
+    //                 return;
+    //             }
+    //             else if((index + 1) % 2 == 0){
+    //                 photo.classList.add('slide-left')
+    //             }
+    //             else{
+    //                 photo.classList.add('slide-right')
+    //             }
+    //         }
+    //     }
+    // }
 
 }, false);
